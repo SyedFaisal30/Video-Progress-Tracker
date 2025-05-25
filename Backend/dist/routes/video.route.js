fbd5cc8184f994e9915db9ad5be95e6bfa3d95cf
SyedFaisal30 <sfarz172320@gmail.com>
2025-05-25 18:00:49 +0530

    Remove dist folder from tracking

--- Backend/dist/routes/video.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const getvideos_controller_1 = require("../controllers/getvideos.controller");
-const getone_video_controller_1 = require("../controllers/getone video.controller");
-const router = (0, express_1.Router)();
-router.get('/', getvideos_controller_1.getVideos);
-router.get('/:id', getone_video_controller_1.getSingleVideo); // GET /api/videos/:id
-exports.default = router;
